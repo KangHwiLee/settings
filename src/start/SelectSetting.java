ef43bbfc6eec98941205dba6066a1a1724fe4710
kanghwi <rkdgnl12336@gmail.com>
2023-02-17 17:56:10 +0900

    집에서 작업하기전 커밋, 아직 준비만 했습니다

--- src/start/SelectSetting.java
+++ src/start/SelectSetting.java
@@ -1,4 +1,19 @@
 package start;
 
+import java.util.Scanner;
+
 public class SelectSetting {
+
+    Scanner sc = new Scanner(System.in);
+
+    public int SelectSetting(){
+
+        System.out.println("설치할 프로그램을 선택해주세요");
+        System.out.println("1:STS , 2:JDK1.8");
+        System.out.print(">>> : ");
+        int s = sc.nextInt();
+        System.out.println(s + "를 선택하셨습니다");
+        return 0;
+    }
+
 }
--- src/start/SettingsStart.java
+++ src/start/SettingsStart.java
@@ -5,8 +5,16 @@ import com.sun.org.slf4j.internal.LoggerFactory;
 import java.util.logging.Logger;
 
 public class SettingsStart {
+    static SelectSetting setting = new SelectSetting();
+
     public static void main(String[] args) {
         System.out.println("test");
 
+        while(true){
+            int test = setting.SelectSetting();
+            if(test == 3){
+                break;
+            }
+        }
     }
 }
