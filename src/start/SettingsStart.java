3ed78604bcf88e98b8c0962069dba7469f0062ef
kanghwi <rkdgnl12336@gmail.com>
2023-02-17 15:36:24 +0900

    해보자고~

---
+++ src/start/SettingsStart.java
@@ -0,0 +1,12 @@
+package start;
+
+import com.sun.org.slf4j.internal.LoggerFactory;
+
+import java.util.logging.Logger;
+
+public class SettingsStart {
+    public static void main(String[] args) {
+        System.out.println("test");
+
+    }
+}
